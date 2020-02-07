5f6c1126d33917ca9d5eccc80ad590acf17a38d8
Altaf Quadri <altafmquadri@gmail.com>
2020-02-07 01:04:31 -0500

    rendered the boxes

--- src/Board.js
+++ src/Board.js
@@ -30,9 +30,15 @@ import './Board.css';
  **/
 
 class Board extends Component {
-
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5
+  }
   constructor(props) {
     super(props);
+    this.state = {
+      board: this.createBoard()
+    }
 
     // TODO: set initial state
   }
@@ -40,8 +46,18 @@ class Board extends Component {
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
 
   createBoard() {
+    let {nrows, ncols} = this.props
+    let random
     let board = [];
     // TODO: create array-of-arrays of true/false values
+    for (let r=0; r < nrows; r++) {
+      board.push([0])
+      for (let c=0; c < ncols; c++) {
+        random = Math.floor(Math.random() * 2)
+        random = random === 1 ? true : false
+        board[r][c] = random
+      }
+    }
     return board
   }
 
@@ -73,17 +89,38 @@ class Board extends Component {
   /** Render game board or winning message. */
 
   render() {
-    return (
-      <div>
-
-      </div>
+    const renderboard = () => {
+      return this.state.board
+      .map((r, index1) =><tr key={index1}>{r
+        .map((c, index) =><Cell key={`${index1+'-'+index}`} isLit={c}/>)}</tr>)
+    }
+    console.log(renderboard())
+      return (
+        
+        <table className="Board">
+        <tbody>
+            {
+              renderboard()
+            }
+          {/* <tr>
+            <Cell isLit={true}/>
+            <Cell isLit={false}/>
+            <Cell isLit={true}/>
+            </tr>
+            <tr>
+            <Cell isLit={true}/>
+            <Cell isLit={false}/>
+            <Cell isLit={true}/>
+          </tr> */}
+        </tbody>
+      </table>
     )
     // if the game is won, just show a winning msg & render nothing else
-
+    
     // TODO
-
+    
     // make table board
-
+    
     // TODO
   }
 }
